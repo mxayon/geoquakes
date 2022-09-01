cdedaecc0990cd8a64719359e4c30b3064739600
mxayon <mxnkpl@gmail.com>
2022-09-01 09:22:17 -0700

    using fetch

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,12 @@
-var weekly_quakes_endpoint =
+let weekly_quakes_endpoint =
   "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_week.geojson";
 
 $(document).ready(function () {
   console.log("Loading!");
 });
+
+fetch(
+  "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_week.geojson"
+)
+  .then((response) => response.json())
+  .then((commits) => alert(commits.metadata.title));
