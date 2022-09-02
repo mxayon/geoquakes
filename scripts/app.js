ef73ccd403cffa7c42f984e55753c04c5911ae84
mxayon <mxnkpl@gmail.com>
2022-09-02 12:47:37 -0700

    unix time stamp converts to human readable hours now

--- scripts/app.js
+++ scripts/app.js
@@ -9,31 +9,30 @@ const url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_week.
 fetch(url)
   .then((response) => response.json())
   .then((data) => {
-    let commits = data;
-    let array_features = commits.features;
-    alert(commits.metadata.title);
-    array_features.map(function(array_feature) {
+    let quakes = data.features;
+    alert(data.metadata.title);
+    quakes.map(function(quake) {
       let li = document.createElement("li");
       let title = document.createElement("h2")
       let place = document.createElement("h3");
-      let unixTime = document.createElement("p");
+      let time = document.createElement("p");
       let status = document.createElement("p");
       let link = document.createElement("a");
-      let originalTime = new Date(array_feature.properties.time * 1000);
-      let hoursSince = originalTime.getHours();
-      let minutesSince = "0" + originalTime.getMinutes();
-      let secondsSince = "0" + originalTime.getSeconds();
-      let formatTime = hoursSince + ":" + minutesSince.substr(-2) + ":" + secondsSince.substr(-2)
+      let hours_since = Math.round(( Date.now() - quake.properties.time ) / (60*60*1000));
+      // let hoursSince = originalTime.getHours();
+      // let minutesSince = "0" + originalTime.getMinutes();
+      // let secondsSince = "0" + originalTime.getSeconds();
+      // let formatTime = hoursSince + ":" + minutesSince.substr(-2) + ":" + secondsSince.substr(-2)
 
-      title.innerHTML = `${array_feature.properties.title}`;
-      place.innerHTML = `${array_feature.properties.place}`;
-      unixTime.innerHTML = `${formatTime}`;
-      status.innerHTML = `${array_feature.properties.status}`;
-      link.innerHTML = `${array_feature.properties.url}`;
+      title.innerHTML = `${quake.properties.title}`;
+      place.innerHTML = `${quake.properties.place}`;
+      time.innerHTML = `${hours_since}` + " hours ago.";
+      status.innerHTML = `${quake.properties.status}`;
+      link.innerHTML = `${quake.properties.url}`;
 
       li.appendChild(title);
       li.appendChild(place);
-      li.appendChild(unixTime);
+      li.appendChild(time);
       li.appendChild(status); 
       li.appendChild(link);
       list.appendChild(li);
