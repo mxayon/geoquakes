338a1ee176d0f80e7d0032330b47c47e1f8f8f2f
mxayon <mxnkpl@gmail.com>
2022-09-04 19:23:53 -0700

    integrating gmaps

--- scripts/app.js
+++ scripts/app.js
@@ -1,9 +1,12 @@
-$(document).ready(function () {
-  console.log("Loading!");
-});
-
 const list = document.createDocumentFragment();
 const url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_week.geojson";
+let map;
+
+$(document).ready(function () {
+  console.log("Loading!");
+  initMap();
+});
+
 
 
 fetch(url)
@@ -38,5 +41,13 @@ fetch(url)
   .catch(function (error) {
     console.log(error);
   });
+
+  function initMap() {
+    console.log("map function");
+    map = new google.maps.Map(document.getElementById("map"), {
+    center: {lat: 37.78, lng: -122.44},
+    zoom: 1,
+  });
+  };
 
 
